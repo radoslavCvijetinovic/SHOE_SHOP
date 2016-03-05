f8460abdeb0b87030134f4070bba7eb03e6c80ad
Radoslav <rajkofon@yahoo.com>
2016-03-05 17:40:10 +0100

    Added spinner to preloader

--- shoe-shop-parent/shoe-shop-fx/src/main/java/com/rcs/shoe/shop/fx/AppPreloader.java
+++ shoe-shop-parent/shoe-shop-fx/src/main/java/com/rcs/shoe/shop/fx/AppPreloader.java
@@ -7,16 +7,19 @@ package com.rcs.shoe.shop.fx;
 
 import javafx.application.Preloader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author Rajko
  */
-public class AppPreloader extends Preloader {    
+public class AppPreloader extends Preloader {
+
     Stage preloaderStage;
     StackPane root;
     Scene preloaderScene;
@@ -25,16 +28,23 @@ public class AppPreloader extends Preloader {
     public void init() throws Exception {
         root = new StackPane();
 
-        root.getStyleClass().add(Constants.MAIN_PANNEL_CLASS);
-        Label label = new Label("Molimo sačekajte...");
-        root.getChildren().add(label);
+        Image spinnerImage = new Image(getClass().getResource(Constants.SPINNER_FILE_PATH).toExternalForm());
+        ImageView iv1 = new ImageView();
+        iv1.setImage(spinnerImage);
+        
+        HBox box = new HBox();
+        box.getChildren().add(iv1);
+        root.getChildren().add(box);
+
         root.getStylesheets().add(Constants.STYLE_FILE_PATH);
         preloaderScene = new Scene(root, Constants.PRELOADER_WIDTH, Constants.PRELOADER_HEIGHT);
+        preloaderScene.setFill(null);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
         preloaderStage = stage;
+        preloaderStage.initStyle(StageStyle.TRANSPARENT);
         preloaderStage.setTitle(Constants.APP_TITLE);
         preloaderStage.getIcons().add(new Image(getClass().getResource(Constants.ICON_FILE_PATH).toExternalForm()));
         preloaderStage.setScene(preloaderScene);
--- shoe-shop-parent/shoe-shop-fx/src/main/java/com/rcs/shoe/shop/fx/Constants.java
+++ shoe-shop-parent/shoe-shop-fx/src/main/java/com/rcs/shoe/shop/fx/Constants.java
@@ -8,12 +8,13 @@ public class Constants {
 
     public static final String STYLE_FILE_PATH = "/styles/styles.css";
     public static final String ICON_FILE_PATH = "/images/shoe-shop-fx.png";
+    public static final String SPINNER_FILE_PATH = "/images/spinner.gif";
     public static final String MAIN_PANNEL_CLASS = "main-panel";
     public static final String APP_TITLE = "Shoe Shop";
     public static final int APP_WIDTH = 800;
     public static final int APP_HEIGHT = 600;
-    public static final int PRELOADER_WIDTH = 300;
-    public static final int PRELOADER_HEIGHT = 250;
+    public static final int PRELOADER_WIDTH = 150;
+    public static final int PRELOADER_HEIGHT = 150;
 
     public static final String LOGIN_FXML = "/fxml/login.fxml";
     public static final String MAIN_FXML = "/fxml/main.fxml";
